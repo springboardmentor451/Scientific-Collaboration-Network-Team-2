a0fbb99de40b7d969967a384524e65b1862138e5
DhanalaxmiRavela <dhanalaxmiravela049@gmail.com>
2026-08-06 09:46:44 +0530

    main changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
+from backend.app.database.connection import test_connection
 
-app = FastAPI()
+app = FastAPI(
+    title="Scientific Collaboration Network Analyzer"
+)
 
 @app.get("/")
-def root():
-    return {"message": "Hello World"}
+def home():
+    return {
+        "message": "Welcome to Scientific Collaboration Network Analyzer"
+    }
+
+@app.get("/db-test")
+def db_test():
+    if test_connection():
+        return {
+            "status": "success",
+            "message": "Database connected successfully!"
+        }
+
+    return {
+        "status": "failed",
+        "message": "Database connection failed!"
+    }
